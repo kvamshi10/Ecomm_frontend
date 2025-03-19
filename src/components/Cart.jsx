b415dec1aa25d40cb37110579b528ba3c0984383
k vamshi krishna <kvamshi10>
2025-03-19 16:53:12 +0530

    second

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
     }
 
     try {
-      const response = await axios.get("http://localhost:5000/cart", {
+      const response = await axios.get("https://ecommerce-backend-87n0.onrender.com/cart", {
         headers: { Authorization: `Bearer ${token}` },
       });
       setCart(response.data);
@@ -36,7 +36,7 @@ const Cart = () => {
 
     try {
       await axios.post(
-        "http://localhost:5000/cart/remove",
+        "https://ecommerce-backend-87n0.onrender.com/cart/remove",
         { productId },
         {
           headers: { Authorization: `Bearer ${token}` },
